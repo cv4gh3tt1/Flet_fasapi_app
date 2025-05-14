0baea06129fe50a4b6df10c2ed315bcaf3b21cc2
cv4gh3tt1 <ch2vaghetti@gmail.com>
2025-05-14 08:11:14 -0300

    Refatora a estrutura do aplicativo para incluir duas páginas: primeira e segunda, com navegação entre elas.

--- app.py
+++ app.py
@@ -2,8 +2,32 @@ import flet as ft
 import flet_fastapi
 
 
-async def main(page: ft.Page):
-    await page.add_async(ft.Text("Olá, Flet!"))
+async def primeira_pagina(page: ft.Page):
+    btn = ft.TextButton(
+        icon=ft.icons.OPEN_IN_NEW,
+        text="Acessar segunda página",
+        url="/segunda-pagina/",
+    )
+    await page.add_async(
+        ft.Text("Primeira página!"),
+        btn,
+    )
 
 
-app = flet_fastapi.app(main)
+async def segunda_pagina(page: ft.Page):
+    btn = ft.TextButton(
+        icon=ft.icons.OPEN_IN_NEW,
+        text="Acessar primeira página",
+        url="/primeira-pagina/",
+    )
+    await page.add_async(
+        ft.Text("Segunda página!"),
+        btn,
+    )
+
+
+app = flet_fastapi.FastAPI()
+
+
+app.mount("/primeira-pagina", flet_fastapi.app(primeira_pagina))
+app.mount("/segunda-pagina", flet_fastapi.app(segunda_pagina))
